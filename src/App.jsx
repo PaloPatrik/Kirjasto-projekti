8137618348a04652b121a73613b13dfb93a72ec5
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2024-03-04 12:18:17 +0200

    päivitys

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
+import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Tabs from '@mui/material/Tabs';
 
 function App() {
+  const [value, setValue] = React.useState(0)
   return (
     <div className="App">
+      <Tabs value={value}>
       <Stack spacing={5} direction = "row">
-        <Button variant="contained">Kirjaudu</Button>
-        <Button variant="outlined">Rekisteröidy</Button>
+        <Button variant="contained" onClick={() => setValue(0)}>Kirjaudu</Button>
+        <Button variant="outlined" onClick={() => setValue(1)}>Rekisteröidy</Button>
       </Stack>
+      </Tabs>
       <Stack spacing={3} direction = "column" id="kirjaudu-kentta">
         <TextField variant="outlined" helperText = "gr tunnus" />
         <TextField variant="outlined" helperText = "salasana" />
