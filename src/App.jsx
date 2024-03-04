0c59eda7c4cc5ec268c374f76b033ca3ccdcb147
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2024-03-04 09:58:45 +0200

    homma aloitettu

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+function App() {
+  return (
+    <div className="App">
+      <Stack spacing={5} direction = "row">
+        <Button variant="contained">Kirjaudu</Button>
+        <Button variant="outlined">Rekisteröidy</Button>
+      </Stack>
+      <Stack spacing={3} direction = "column" id="kirjaudu-kentta">
+        <TextField variant="outlined" helperText = "gr tunnus" />
+        <TextField variant="outlined" helperText = "salasana" />
+      </Stack>
+    </div>
+  );
+}
+
+export default App;
