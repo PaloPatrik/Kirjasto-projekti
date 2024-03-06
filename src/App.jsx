cbfc755d8b074dc3e2fd5318ddd6f23b1ed43214
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2024-03-06 10:35:48 +0200

    edistys

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,39 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Tabs from '@mui/material/Tabs';
 
 function App() {
   const [value, setValue] = React.useState(0)
+  if (value === 1) {
+    return (
+      <div>
+        
+        <Stack spacing={5} direction = "row">
+        <Button variant="contained" onClick={() => setValue(0)}>Kirjaudu</Button>
+        <Button variant="outlined" onClick={() => setValue(1)}>Rekisteröidy</Button>
+        </Stack>
+        
+        <Stack spacing={3} direction="column" id="rekisteroidy-kentta">
+          <TextField variant='outlined' helperText='nimi'/>
+          <TextField variant='outlined' helperText='gr tunnus'/>
+          <TextField variant='outlined' helperText='salasana'/>
+          <TextField variant='outlined' helperText='salasana uudelleen'/>
+        </Stack>
+        <Stack>
+          <Button variant='contained'>Rekisteröidy</Button>
+        </Stack>
+      </div>
+    )
+  }
+  else
   return (
     <div className="App">
-      <Tabs value={value}>
+      
       <Stack spacing={5} direction = "row">
         <Button variant="contained" onClick={() => setValue(0)}>Kirjaudu</Button>
         <Button variant="outlined" onClick={() => setValue(1)}>Rekisteröidy</Button>
       </Stack>
-      </Tabs>
+      
       <Stack spacing={3} direction = "column" id="kirjaudu-kentta">
         <TextField variant="outlined" helperText = "gr tunnus" />
         <TextField variant="outlined" helperText = "salasana" />
