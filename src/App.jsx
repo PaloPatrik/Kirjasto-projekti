f74a69fca6cf786f88792f7d45e5c797f0bd8fda
Empnor <eemeli.noronen@gmail.com>
2024-03-08 09:47:29 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import './App.css';
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Tabs from '@mui/material/Tabs';
-import FormControl from '@mui/material/FormControl';
-import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import React from 'react';
+import header from './kirjastocut.png';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
+
+
 
 function App() {
- 
   return (
-    <div className="App">
-      <div class= "apua">
-      <TextareaAutosize minRows={4} variant="outlined" helperText = "kerro ongelmasta" />
-</div>
+    <div>
+      <header style={{ textAlign: 'center' }}>
+  <img src={header} alt="header" style={{ width: '100%' }} />
+</header>
 
-<RadioGroup
-        row 
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '50vh', 
+          overflow: 'hidden',
+        }}
       >
-        <FormControlLabel value="kirjasto1" control={<Radio />} label="kirjasto1" />
-        <FormControlLabel value="kirjasto2" control={<Radio />} label="kirjasto2" />
-        <FormControlLabel value="kirjasto3" control={<Radio />} label="kirjasto3" />
-      </RadioGroup>
+        
+
+        <RadioGroup row name="kirjasto" style={{ marginBottom: '20px' }}>
+          <FormControlLabel value="kirjasto1" control={<Radio />} label="kirjasto1" />
+          <FormControlLabel value="kirjasto2" control={<Radio />} label="kirjasto2" />
+          <FormControlLabel value="kirjasto3" control={<Radio />} label="kirjasto3" />
+        </RadioGroup>
+
+        <TextareaAutosize
+          minRows={4}
+          placeholder="Kerro ongelmasta"
+          style={{ width: '300px', maxWidth: '100%', marginTop: '10px', marginBottom: '20px'}}
+        />
+        <Button variant="contained"
+        style={{marginTop: '20px'}}
+        >Lähetä</Button>
+      </div>
     </div>
   );
 }
