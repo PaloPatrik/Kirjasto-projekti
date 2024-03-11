cdbdf4d56ee8b8743c4c8f1c554d8bc73d97b6ec
Empnor <eemeli.noronen@gmail.com>
2024-03-11 13:26:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,12 @@ import TextField from '@mui/material/TextField';
 
 
 
-
 function App() {
   return (
     <div>
-      <header style={{ textAlign: 'center' }}>
+<header style={{ textAlign: 'center' }}>
   <img src={header} alt="header" style={{ width: '100%' }} />
 </header>
-
-
       <div
         style={{
           display: 'flex',
@@ -35,15 +32,21 @@ function App() {
           <FormControlLabel value="kirjasto3" control={<Radio />} label="kirjasto3" />
         </RadioGroup>
 
+        <RadioGroup row name="laite" style={{ marginBottom: '20px' }}>
+          <FormControlLabel value="Tietokone" control={<Radio />} label="Tietokone" />
+          <FormControlLabel value="Puhelin" control={<Radio />} label="Puhelin" />
+          <FormControlLabel value="Mikroaaltouuni" control={<Radio />} label="Mikroaaltouuni" />
+          <FormControlLabel value="Pankkitili" control={<Radio />} label="Pankkitili" />
+          <FormControlLabel value="Tabletti" control={<Radio />} label="Tabletti" />
+          <FormControlLabel value="mikroaaltouun" control={<Radio />} label="mikroaaltouuni" />
+        </RadioGroup>
     <TextField
       multiline
       rows={4}
-      placeholder="Enter text here"
+      placeholder="Kerro paikastasi"
       variant="outlined"
       fullWidth
-      
-    />
-        
+      />
         <Button variant="contained"
         style={{marginTop: '20px'}}
         >Lähetä</Button>
