a34777ddbb9a9eede68436f735b7d290f254b947
Empnor <eemeli.noronen@gmail.com>
2024-03-25 13:39:37 +0200

    css lisätty

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,54 @@
-import './App.css';
 import React from 'react';
-import header from './kirjastocut.png';
+import Typography from '@mui/material/Typography';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-
-
+import header from './kirjastocut.png';
+import './App.css';
 
 function App() {
   return (
     <div>
-<header style={{ textAlign: 'center' }}>
-  <img src={header} alt="header" style={{ width: '100%' }} />
-</header>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          overflow: 'hidden',
-        }}
-      >
+      <header>
+        <img src={header} alt="header" />
+      </header>
+      <div className="form-container">
         <Typography variant="h4" gutterBottom>
-        missä kirjastolla olit töissä
-      </Typography>
-        <RadioGroup row name="kirjasto" style={{ marginBottom: '20px' }}>
-          <FormControlLabel value="kirjasto1" control={<Radio />} label="kirjasto1" />
-          <FormControlLabel value="kirjasto2" control={<Radio />} label="kirjasto2" />
-          <FormControlLabel value="kirjasto3" control={<Radio />} label="kirjasto3" />
+          Missä kirjastolla olit töissä
+        </Typography>
+        <RadioGroup row name="kirjasto" className="radio-group">
+          <FormControlLabel value="kirjasto1" control={<Radio />} label="Kirjasto1"/>
+          <FormControlLabel value="kirjasto2" control={<Radio />} label="Kirjasto2"/>
+          <FormControlLabel value="kirjasto3" control={<Radio />} label="Kirjasto3"/>
         </RadioGroup>
         <Typography variant="h4" gutterBottom>
-        missä oli vika
-      </Typography>
-        <RadioGroup row name="laite" style={{ marginBottom: '20px' }}>
-          <FormControlLabel value="Tietokone" control={<Radio />} label="Tietokone" />
-          <FormControlLabel value="Puhelin" control={<Radio />} label="Puhelin" />
-          <FormControlLabel value="Mikroaaltouuni" control={<Radio />} label="Mikroaaltouuni" />
-          <FormControlLabel value="Pankkitili" control={<Radio />} label="Pankkitili" />
-          <FormControlLabel value="Tabletti" control={<Radio />} label="Tabletti" />
-          <FormControlLabel value="mikroaaltouun" control={<Radio />} label="mikroaaltouuni" />
+          Missä oli vika
+        </Typography>
+        <RadioGroup row name="laite" className="radio-group">
+          <FormControlLabel value="Tietokone" control={<Radio />} label="Tietokone"/>
+          <FormControlLabel value="Puhelin" control={<Radio />} label="Puhelin"/>
+          <FormControlLabel value="Mikroaaltouuni" control={<Radio />} label="Mikroaaltouuni"/>
+          <FormControlLabel value="Pankkitili" control={<Radio />} label="Pankkitili"/>
+          <FormControlLabel value="Tabletti" control={<Radio />} label="Tabletti"/>
+          <FormControlLabel value="Muu" control={<Radio />} label="Muu"/>
         </RadioGroup>
-    <TextField
-      multiline
-      rows={4}
-      placeholder="lisä tietoa"
-      variant="outlined"
-      fullWidth
-      />
-        <Button variant="contained"
-        style={{marginTop: '20px'}}
-        >Lähetä</Button>
+        <TextField 
+          className="teksti"
+          multiline
+          rows={4}
+          placeholder="Lisätietoja"
+          variant="outlined"
+          fullWidth
+          
+        />
+        <Button variant="contained" className="Buttoni">
+          Lähetä
+        </Button>
       </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
