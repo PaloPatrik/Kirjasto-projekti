36f6fe9dd85dc1ab5fc4f664c2fb6714bb97deaf
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2024-04-05 10:32:23 +0300

    tiedot näkyy

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,6 +3,7 @@ import './Content.css';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 const Content = () => {
     return (
@@ -16,15 +17,18 @@ const Content = () => {
         </div>
 
         <div className='Information'>
-            <Stack>
-            <Stack className='personProb' direction='row' spacing={15}>
+            <Stack className='pidor'>
+            <Stack className='personProb' direction='column' spacing={3}>
                 <Typography>Henkilö:</Typography>
                 <Typography>Päivämäärä:</Typography>
-            </Stack>
-
-            <Stack className='personProb2' direction='row' spacing={16.5}>
-                <Typography>Päivämäärä:</Typography>
+                <Typography>Ongelma:</Typography>
                 <Typography>Tiedot:</Typography>
+                <TextField
+                    multiline
+                    rows={4}
+                    className='Lisatiedot'
+                    disabled
+                />
             </Stack>
             </Stack>
         </div>
