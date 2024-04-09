a337a7bae14a141c23a99fd1f9d4b3094fb04169
Empnor <eemeli.noronen@gmail.com>
2024-04-09 09:07:54 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,46 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <form>
       <header> 
-        <img src={header} alt="header" />
-      </header>
+        <img src={header} alt="header" /> 
+      </header> 
       <div className="FormContainer">
         <Typography variant="h4" gutterBottom>
           Missä kirjastolla olit töissä
         </Typography>
-        <RadioGroup row name="kirjasto" className="radio-group">
-          <FormControlLabel value="kirjasto1" control={<Radio />} label="Kirjasto1"/>
+        <RadioGroup row name="kirjasto" className="RadioGroup">
+          <FormControlLabel value="kirjasto1" control={<Radio />} label="Kirjasto1"/> 
           <FormControlLabel value="kirjasto2" control={<Radio />} label="Kirjasto2"/>
           <FormControlLabel value="kirjasto3" control={<Radio />} label="Kirjasto3"/>
         </RadioGroup>
         <Typography variant="h4" gutterBottom>
           Missä oli vika
         </Typography>
-        <RadioGroup row name="laite" className="radio-group">
+        <RadioGroup row name="laite" className="RadioGroup"
+        //radiogroup missä oli vika
+        >
           <FormControlLabel value="Tietokone" control={<Radio />} label="Tietokone"/>
           <FormControlLabel value="Puhelin" control={<Radio />} label="Puhelin"/>
-          <FormControlLabel value="Mikroaaltouuni" control={<Radio />} label="Mikroaaltouuni"/>
+          <FormControlLabel value="Mikroaaltouuni" control={<Radio />} label="Mikroaaltouuni" /> 
           <FormControlLabel value="Pankkitili" control={<Radio />} label="Pankkitili"/>
           <FormControlLabel value="Tabletti" control={<Radio />} label="Tabletti"/>
           <FormControlLabel value="Muu" control={<Radio />} label="Muu"/>
-        </RadioGroup>
+          </RadioGroup>
+     
         <TextField 
-          className="teksti"
-          multiline
-          rows={4}
-          placeholder="Lisätietoja"
-          variant="outlined"
-          fullWidth
-          
+          className="teksti" //classnimi = css tiedoston tyyli
+          multiline 
+          rows={4} //alku rivit
+          placeholder="Lisätietoja" //placeholder = mitä näkyy ennenkuin tekstiä on laitettu
+          variant="outlined" // outlinattu
+          fullWidth //koko alueen vie 
         />
         <Button variant="contained" className="Buttoni">
           Lähetä
         </Button>
       </div>
-    </div>
+    </form>
   ); 
 }
 
